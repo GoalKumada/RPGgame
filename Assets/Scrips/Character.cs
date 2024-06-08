154e7ddaacfdbf0bec8c486d92473e7af465b093
GoalKumada <goal.k82898@gmail.com>
2024-06-08 16:42:42 +0900

    スクリプト作成

---
+++ Assets/Scrips/Character.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private float HP;
+    [SerializeField]
+    private float TP;
+    [SerializeField]
+    private float DEF;
+    [SerializeField]
+    private float SPEED;
+
+    private void Start()
+    {
+        
+
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void attack()
+    {
+
+
+
+    }
+
+    public void useSkill()
+    {
+
+    }
+}
