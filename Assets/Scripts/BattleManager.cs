ba2b5f42c53aba24cc1b1750311fc60649baf7bc
GoalKumada <goal.k82898@gmail.com>
2024-07-17 21:15:38 +0900

    UIウィンドウの一連の流れを作成

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
     // フェーズの管理
 
+    [SerializeField] WindowMenu chooseRunOrBattleWindowMenu = default;
+    [SerializeField] WindowMenu runCheckWindowMenu = default;
+    [SerializeField] WindowMenu chooseAllyWindowMenu = default;
+    [SerializeField] WindowMenu chooseCommandWindowMenu = default;
+    [SerializeField] WindowMenu chooseEnemyWindowMenu = default;
+    
     enum Phase
     {
         StartPhase,
         ChooseRunOrBattlePhase, //「たたかう」か「にげる」か選ぶ
-        ChooseArrayPhase, // 行動する味方を選ぶ
+        RunCheckPhase, // 本当ににげるかどうかの確認
+        ChooseAllyPhase, // 行動する味方を選ぶ
         ChooseCommandPhase, //コマンドを選ぶ
         ChooseEnemyPhase, // コマンドを実行する相手を選ぶ
         ExecutePhase,
@@ -35,42 +42,132 @@ public class BattleManager : MonoBehaviour
             switch (phase)
             {
                 case Phase.StartPhase:
-                    //
-                    yield return new WaitUntil(() => Keyboard.current.spaceKey.wasReleasedThisFrame);
+                                        
+                    //yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+                    
                     phase = Phase.ChooseRunOrBattlePhase;
+                    // ウィンドウをアクティブにする
+                    UI.WindowActivated();
+
+
                     break;
+                
                 case Phase.ChooseRunOrBattlePhase:
-                    //
-                    yield return new WaitUntil(() => Keyboard.current.spaceKey.wasReleasedThisFrame);
-                    phase = Phase.ChooseArrayPhase;
+                    
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+                    
+                    int currentID_cROB = chooseRunOrBattleWindowMenu.currentID;
+
+
+                    
+                    if (currentID_cROB == 0) // 0(たたかう)ならChooseAllyPhaseへ
+                    {
+                        phase = Phase.ChooseAllyPhase;
+
+                    }
+                    else // 1（にげる）ならRunCheckへ
+                    {
+                        phase = Phase.RunCheckPhase;
+
+                    }
                     break;
-                case Phase.ChooseArrayPhase:
-                    //
-                    yield return new WaitUntil(() => Keyboard.current.spaceKey.wasReleasedThisFrame);
-                    phase = Phase.ChooseCommandPhase;
+                
+                case Phase.RunCheckPhase:
+                    
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
+                    int currentID_rC = runCheckWindowMenu.currentID;
+
+                    if (currentID_rC == 0)
+                    {
+                        phase = Phase.ChooseRunOrBattlePhase;
+
+                    }
+                    else
+                    {
+                        phase = Phase.StartPhase;
+
+                    }
                     break;
+                
+                case Phase.ChooseAllyPhase:
+                    
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+                    
+                    int currentID_cA = chooseAllyWindowMenu.currentID;
+
+                    if (currentID_cA == 0)
+                    {
+                        phase = Phase.ChooseCommandPhase;
+                    }
+                    else if (currentID_cA == 1)
+                    {
+                        phase = Phase.ChooseCommandPhase;
+                    }
+                    else
+                    {
+                        phase = Phase.ChooseCommandPhase;
+                    }
+                    break;
+                
                 case Phase.ChooseCommandPhase:
-                    //
-                    yield return new WaitUntil(() => Keyboard.current.spaceKey.wasReleasedThisFrame);
-                    phase = Phase.ChooseEnemyPhase;
+                    
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
+                    int currentID_cC = chooseCommandWindowMenu.currentID;
+
+                    if (currentID_cC == 0)
+                    {
+                        phase = Phase.ChooseEnemyPhase;
+                    }
+                    else if (currentID_cC == 1)
+                    {
+                        phase = Phase.ChooseEnemyPhase;
+                    }
+                    else
+                    {
+                        phase = Phase.ChooseEnemyPhase;
+                    }
                     break;
+                
                 case Phase.ChooseEnemyPhase:
-                    //
-                    yield return new WaitUntil(() => Keyboard.current.spaceKey.wasReleasedThisFrame);
-                    phase = Phase.ExecutePhase;
+                    
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
+                    int currentID_cE = chooseEnemyWindowMenu.currentID;
+
+                    if (currentID_cE == 0)
+                    {
+                        phase = Phase.ExecutePhase;
+                    }
+                    else if (currentID_cE == 1)
+                    {
+                        phase = Phase.ExecutePhase;
+                    }
+                    else
+                    {
+                        phase = Phase.ExecutePhase;
+                    }
                     break;
+                
                 case Phase.ExecutePhase:
-                    //
-                    yield return new WaitUntil(() => Keyboard.current.spaceKey.wasReleasedThisFrame);
+                    
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
                     phase = Phase.Result;
                     break;
+                
                 case Phase.Result:
-                    //
-                    yield return new WaitUntil(() => Keyboard.current.spaceKey.wasReleasedThisFrame);
+                    
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
                     phase = Phase.End;
                     break;
+                
                 case Phase.End:
-                    //
+
+                    phase = Phase.StartPhase;
+                    
                     break;
 
             }
---
+++ Assets/Scripts/Window/WindowMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class WindowMenu : MonoBehaviour
+{
+
+    // SelectableTextが選択されたら、カーソルを移動する（親オブジェクトを変更する）
+
+    [SerializeField] Transform arrow = default;
+    [SerializeField] List<SelectableText> selectableTexts = new List<SelectableText>();
+
+    public int currentID; // 選択中の子要素のID
+
+    private void Start()
+    {
+        // test
+        SetMoveArrowFunction();
+
+    }
+
+
+
+    void SetMoveArrowFunction()
+    {
+        foreach (SelectableText selectableText in selectableTexts) 
+        {
+            selectableText.onSelectAction = MoveArrowTo;
+        }
+
+        // 最初から一番上の選択肢を選択状態にする
+        EventSystem.current.SetSelectedGameObject(selectableTexts[currentID].gameObject);
+    }
+    
+    // 親を変更してカーソルを移動する
+    public void MoveArrowTo(Transform parent)
+    {
+        arrow.SetParent(parent);
+        currentID = parent.GetSiblingIndex(); // 何番目の子要素かを取得
+        Debug.Log($"カーソル移動:currentID{currentID}");
+    }
+}
